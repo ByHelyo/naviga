6542324f30832ad149a9ae3f81c02684d8fe420c
Franck Hu <franck.hu@epita.fr>
2022-08-02 03:34:20 +0200

    add movements in key handler

--- src/app.rs
+++ src/app.rs
@@ -8,16 +8,25 @@ use tui::widgets::{Block, Borders, Paragraph};
 /// Application result type.
 pub type AppResult<T> = std::result::Result<T, Box<dyn error::Error>>;
 
+#[derive(Debug)]
+pub enum Action {
+    Left,
+    Right,
+    Up,
+    Down,
+}
+
 /// Application.
 #[derive(Debug)]
 pub struct App {
     /// Is the application running?
     pub running: bool,
+    pub key: Option<Action>,
 }
 
 impl Default for App {
     fn default() -> Self {
-        Self { running: true }
+        Self { running: true, key: None }
     }
 }
 
@@ -36,12 +45,53 @@ impl App {
         // See the following resources:
         // - https://docs.rs/tui/0.16.0/tui/widgets/index.html
         // - https://github.com/fdehau/tui-rs/tree/v0.16.0/examples
-        frame.render_widget(
-            Paragraph::new("naviga")
-                .block(Block::default().borders(Borders::ALL))
-                .style(Style::default().fg(Color::White).bg(Color::Black))
-                .alignment(Alignment::Center),
-            frame.size(),
-        )
+
+        match self.key {
+            Some(Action::Left) => {
+                frame.render_widget(
+                    Paragraph::new("left")
+                        .block(Block::default().borders(Borders::ALL))
+                        .style(Style::default().fg(Color::White).bg(Color::Black))
+                        .alignment(Alignment::Center),
+                    frame.size(),
+                );
+            }
+            Some(Action::Right) => {
+                frame.render_widget(
+                    Paragraph::new("Right")
+                        .block(Block::default().borders(Borders::ALL))
+                        .style(Style::default().fg(Color::White).bg(Color::Black))
+                        .alignment(Alignment::Center),
+                    frame.size(),
+                );
+            }
+            Some(Action::Up) => {
+                frame.render_widget(
+                    Paragraph::new("Up")
+                        .block(Block::default().borders(Borders::ALL))
+                        .style(Style::default().fg(Color::White).bg(Color::Black))
+                        .alignment(Alignment::Center),
+                    frame.size(),
+                );
+            }
+            Some(Action::Down) => {
+                frame.render_widget(
+                    Paragraph::new("Down")
+                        .block(Block::default().borders(Borders::ALL))
+                        .style(Style::default().fg(Color::White).bg(Color::Black))
+                        .alignment(Alignment::Center),
+                    frame.size(),
+                );
+            }
+            _ => {
+                frame.render_widget(
+                    Paragraph::new("naviga")
+                        .block(Block::default().borders(Borders::ALL))
+                        .style(Style::default().fg(Color::White).bg(Color::Black))
+                        .alignment(Alignment::Center),
+                    frame.size(),
+                );
+            }
+        }
     }
 }
--- src/handler.rs
+++ src/handler.rs
@@ -1,5 +1,6 @@
 use crate::app::{App, AppResult};
 use crossterm::event::{KeyCode, KeyEvent, KeyModifiers};
+use crate::app::Action;
 
 /// Handles the key events and updates the state of [`App`].
 pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
@@ -13,6 +14,22 @@ pub fn handle_key_events(key_event: KeyEvent, app: &mut App) -> AppResult<()> {
             if key_event.modifiers == KeyModifiers::CONTROL {
                 app.running = false;
             }
+        }
+        // Move cursor left
+        KeyCode::Char('h') | KeyCode::Left => {
+            app.key = Some(Action::Left);
+        }
+        // Move cursor right
+        KeyCode::Char('l') | KeyCode::Right => {
+            app.key = Some(Action::Right);
+        }
+        // Move cursor up
+        KeyCode::Char('k') | KeyCode::Up => {
+            app.key = Some(Action::Up);
+        }
+        // Move cursor down
+        KeyCode::Char('j') | KeyCode::Down => {
+            app.key = Some(Action::Down);
         }
         _ => {}
     }
